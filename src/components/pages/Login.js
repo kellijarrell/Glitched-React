f53c5db556e793d4295d776a0daad108df6245ee
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-09-11 12:38:51 -0400

    login page styling

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "../assets/login.css";
+import logo from "../assets/images/glitched-logo.svg"
 
 function Login(props) {
 
@@ -8,11 +9,22 @@ function Login(props) {
     const password = useRef();
 
     return (
-        <div className="mx-auto card" style={{ margin: "40px", maxWidth: "500px" }}>
+        <div className="mx-auto card" style={{ margin: "40px", maxWidth: "500px", backgroundColor: "#71cae3", borderRadius: "10px" }}>
 
-            <div style={{margin: "20px 20px 0px 20px"}}>
+            <div style={{ margin: "20px 20px 0px 20px" }}>
 
+                <div style={{textAlign: "center", margin: "20px"}}>
+                <h1 style={{color: "white"}}>Welcome To</h1>
+                    <img src={logo}
+                        className="mx-auto"
+                        style={{maxWidth: "200px", margin: "10px"}}>
+                    </img>
+                    <h5 style={{color: "white"}}>Please Sign in to find your player 2!</h5>
+
+                </div>
                 <div className="form-group" >
+
+
 
                     <input type="email"
                         className="form-control"
@@ -32,19 +44,19 @@ function Login(props) {
 
                 </div>
                 <div className="row">
-                    <a href="" style={{ color: "black" }}>
+                    <a href="" style={{ color: "white" }}>
                         <p className="col">
                             Forgot Password?</p>
                     </a>
-                    <a href="/Glitched-React/SignUp" style={{ color: "black" }}>
+                    <a href="/Glitched-React/SignUp" style={{ color: "white" }}>
                         <p className="col">
                             New User? Sign Up Here!</p>
                     </a>
                 </div>
-                <Link to = "/Glitched-React/" >
-                <button type="submit" className="btn btn-info" 
-                onClick={() => props.signInUser(email.current.value, password.current.value)} style={{ marginBottom: "20px" }}>
-                    Login
+                <Link to="/Glitched-React/" >
+                    <button type="submit" className="btn btn-outline-light"
+                        onClick={() => props.signInUser(email.current.value, password.current.value)} style={{ marginBottom: "20px" }}>
+                        Login
                 </button>
                 </Link>
 
